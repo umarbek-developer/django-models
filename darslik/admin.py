4efacaf309b52e7d927a95f560a98c37bd1742d6
Umarsobirov <umarbeksobirov0402@mail.ru>
2026-02-26 21:03:37 +0500

    codes

--- darslik/admin.py
+++ darslik/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Universitet, Teacher, Student
+
+admin.site.register(Universitet)
+admin.site.register(Teacher)
+admin.site.register(Student)
+
--- darslik/models.py
+++ darslik/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+class Universitet(models.Model):
+    nom = models.CharField(max_length=255)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    students_count = models.SmallIntegerField(default=0)
+    director_name = models.CharField(max_length=255, blank=True, null=True)
+    image = models.ImageField(upload_to='university-images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.nom}, {self.students_count}"
+
+
+
+    class Meta:
+        verbose_name = "Universitet"
+        verbose_name_plural = "Universitetlar"
+        ordering = ['created_at', 'nom']
+
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=255)
+    bio = models.TextField(blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatar/', blank=True, null=True)
+    experience_year = models.PositiveSmallIntegerField(default=0)
+    salary = models.PositiveBigIntegerField(default=0)
+    groups_count = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.name} {self.salary}"
+
+
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+    bio = models.TextField(blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatar/', blank=True, null=True)
+    gpa = models.FloatField(default = 0)
+    age = models.PositiveSmallIntegerField(default=0)
+    address = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} {self.age}"
+    
+
+
